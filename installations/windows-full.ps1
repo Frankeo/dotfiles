983e0caf9258983dd06407726858df9e8309ea61
Frankeo <ingfrancisco.moreno1@gmail.com>
2024-09-21 21:17:43 +0200

    testing windows

--- installations/windows-full.ps1
+++ installations/windows-full.ps1
@@ -2,7 +2,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force;
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Install Google Chrome
-choco install googlechrome -y
+# choco install googlechrome -y
 
 # Install Docker Desktop
 # choco install docker-desktop -y
@@ -10,12 +10,13 @@ choco install googlechrome -y
 # Install Hyper
 choco install hyper -y
 
-# Install Git
+# Install Git and Git Bash
 choco install git -y
 
 # Install Oh My Zsh
 
 # Clone dorfiles repo
+Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-i', '-l', '-c', '"echo $HOME"') -NoNewWindow -Wait
 Invoke-WebRequest "https://github.com/Frankeo/dotfiles/archive/refs/heads/main.zip" -OutFile "~\repo.zip"
 Expand-Archive -Path "~\repo.zip" -DestinationPath "~\temp"
 Move-Item -Path "~\temp\dotfiles-main" -Destination "~"
@@ -26,7 +27,7 @@ New-Item -ItemType SymbolicLink -Path "~\.gitconfig" -Target "~\dotfiles\git\.gi
 New-Item -ItemType SymbolicLink -Path "~\.hyper.js" -Target "~\dotfiles\hyper\.hyper.js"
 
 # Install VS Code
-choco install vscode -y
+# choco install vscode -y
 
 # Install Common Tools
-choco install audacity vlc gimp -y
+# choco install audacity vlc gimp -y
