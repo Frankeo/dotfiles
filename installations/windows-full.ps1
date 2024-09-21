e2033c3224ed0b1042e3c1f7fb25275becc94db1
Frankeo <ingfrancisco.moreno1@gmail.com>
2024-09-21 20:51:16 +0200

    uncomment install lines in windows powershell

--- installations/windows-full.ps1
+++ installations/windows-full.ps1
@@ -2,7 +2,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force;
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Install Google Chrome
-# choco install googlechrome -y
+choco install googlechrome -y
 
 # Install Docker Desktop
 # choco install docker-desktop -y
@@ -19,14 +19,14 @@ choco install git -y
 Invoke-WebRequest "https://github.com/Frankeo/dotfiles/archive/refs/heads/main.zip" -OutFile "~\repo.zip"
 Expand-Archive -Path "~\repo.zip" -DestinationPath "~\temp"
 Move-Item -Path "~\temp\dotfiles-main" -Destination "~"
-Rename-Item -path "~\dotfiles-main" -NewName "~\dotfiles"
+Rename-Item -path "~\dotfiles-main" -NewName "dotfiles"
 
 # Setup Configs
-New-Item -ItemType SymbolicLink -Path "~\dotfiles\git\.gitconfig" -Target "~\.gitconfig"
-New-Item -ItemType SymbolicLink -Path "~\dotfiles\hyper\.hyper.js" -Target "~\.hyper.js"
+New-Item -ItemType SymbolicLink -Path "~\.gitconfig" -Target "~\dotfiles\git\.gitconfig"
+New-Item -ItemType SymbolicLink -Path "~\.hyper.js" -Target "~\dotfiles\hyper\.hyper.js"
 
 # Install VS Code
-# choco install vscode -y
+choco install vscode -y
 
 # Install Common Tools
-# choco install audacity vlc gimp -y
+choco install audacity vlc gimp -y
