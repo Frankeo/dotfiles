0c89f8f1539262159ca71bfc37525f17fd3651ad
Frankeo <ingfrancisco.moreno1@gmail.com>
2024-09-22 00:31:08 +0200

    Testing adding fonts and fix hyper config

--- installations/windows-full.ps1
+++ installations/windows-full.ps1
@@ -23,7 +23,8 @@ Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c
 
 # Setup Configs
 New-Item -ItemType SymbolicLink -Path "~\.gitconfig" -Target "~\dotfiles\git\.gitconfig"
-New-Item -ItemType SymbolicLink -Path "~\.hyper.js" -Target "~\dotfiles\hyper\.hyper-windows.js"
+Remove-Item "$Env:AppData/Hyper/.hyper.js"
+New-Item -ItemType SymbolicLink -Path "$Env:AppData/Hyper/.hyper.js" -Target "~\dotfiles\hyper\.hyper-windows.js"
 # Setup Oh My Zsh config
 Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c', '"git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k"') -NoNewWindow -Wait
 Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c', '"git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"') -NoNewWindow -Wait
@@ -33,10 +34,22 @@ New-Item -ItemType SymbolicLink -Path "~\.zshrc" -Target "~\dotfiles\oh-my-zsh\.
 New-Item -ItemType SymbolicLink -Path "~\.p10k.zsh " -Target "~\dotfiles\oh-my-zsh\.p10k.zsh"
 
 # Install Fonts
-Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c', '"(cd C:/Windows/Fonts && curl -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf)"') -NoNewWindow -Wait
-Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c', '"(cd C:/Windows/Fonts && curl -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf)"') -NoNewWindow -Wait
-Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c', '"(cd C:/Windows/Fonts && curl -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf)"') -NoNewWindow -Wait
-Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c', '"(cd C:/Windows/Fonts && curl -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf)"') -NoNewWindow -Wait
+Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c', '"(curl -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf)"') -NoNewWindow -Wait
+Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c', '"(curl -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf)"') -NoNewWindow -Wait
+Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c', '"(curl -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf)"') -NoNewWindow -Wait
+Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c', '"(curl -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf)"') -NoNewWindow -Wait
+
+echo "Install fonts"
+$fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
+foreach ($file in gci *.ttf)
+{
+    $fileName = $file.Name
+    if (-not(Test-Path -Path "C:\Windows\fonts\$fileName" )) {
+        echo $fileName
+        dir $file | %{ $fonts.CopyHere($_.fullname) }
+    }
+}
+cp *.ttf c:\windows\fonts\
 
 # Install VS Code
 # choco install vscode -y
