15c5a695529801bb67056b990068712cb3cc8435
Frankeo <ingfrancisco.moreno1@gmail.com>
2024-09-21 19:30:34 +0200

    testing repo cloning and soft links on windows

--- installations/windows-full.ps1
+++ installations/windows-full.ps1
@@ -2,10 +2,10 @@ Set-ExecutionPolicy Bypass -Scope Process -Force;
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Install Google Chrome
-choco install googlechrome -y
+# choco install googlechrome -y
 
 # Install Docker Desktop
-choco install docker-desktop -y
+# choco install docker-desktop -y
 
 # Install Hyper
 choco install hyper -y
@@ -16,14 +16,15 @@ choco install git -y
 # Install Oh My Zsh
 
 # Clone dorfiles repo
-git clone https://github.com/Frankeo/dotfiles.git ~/dotfiles
+Invoke-WebRequest "https://github.com/Frankeo/dotfiles/archive/refs/heads/main.zip" -OutFile "C:\Temp\repo.zip"
+Expand-Archive -Path "C:\Temp\repo.zip" -DestinationPath "~/dotfiles"
 
 # Setup Configs
 New-Item -ItemType SymbolicLink -Path "~/dotfiles/git/.gitconfig" -Target "~/.gitconfig"
 New-Item -ItemType SymbolicLink -Path "~/dotfiles/hyper/.hyper.js" -Target "~/.hyper.js"
 
 # Install VS Code
-choco install vscode -y
+# choco install vscode -y
 
 # Install Common Tools
-choco install audacity vlc gimp -y
+# choco install audacity vlc gimp -y
