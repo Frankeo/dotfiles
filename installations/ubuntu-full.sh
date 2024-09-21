3450d45243488f2b3557fbfa70602496a7bab1ef
Frankeo <ingfrancisco.moreno1@gmail.com>
2024-09-21 13:00:53 +0200

    add yes to docker desktop

--- installations/ubuntu-full.sh
+++ installations/ubuntu-full.sh
@@ -23,7 +23,7 @@ echo \
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 wget https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb
-sudo apt-get install ./docker-desktop-amd64.deb
+sudo apt-get install ./docker-desktop-amd64.deb -y
 rm -rf docker-desktop-amd64.deb
 
 # Clone dotfiles repo
