960b3065f18fef68f948cd37b82de3d689870cfb
Frankeo <ingfrancisco.moreno1@gmail.com>
2024-09-21 22:42:28 +0200

    testing zsh config

--- installations/windows-full.ps1
+++ installations/windows-full.ps1
@@ -14,10 +14,18 @@ choco install hyper -y
 choco install git -y
 
 # Clone dorfiles repo
-Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-i', '-l', '-c', 'git clone https://github.com/Frankeo/dotfiles.git ~/dotfiles') -NoNewWindow -Wait
+Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c', '"git clone https://github.com/Frankeo/dotfiles.git ~/dotfiles"') -NoNewWindow -Wait
 
 # Install Oh My Zsh
-Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-i', '-l', '-c', 'cp -r ~/dotfiles/oh-my-zsh/zsh-5.9-2-x86_64 C:/Program Files/Git') -NoNewWindow -Wait
+Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c', '"cp -r ~/dotfiles/oh-my-zsh/zsh-5.9-2-x86_64/* C:/''Program Files''/Git"') -NoNewWindow -Wait
+$multiLineText = @"
+if [ -t 1 ]; then
+  exec zsh
+fi
+"@
+$multiLineText | Out-File -FilePath "~\.bashrc"
+Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c', '"echo yes | sh -c ''$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)''"') -NoNewWindow -Wait
+
 
 # Setup Configs
 New-Item -ItemType SymbolicLink -Path "~\.gitconfig" -Target "~\dotfiles\git\.gitconfig"
