2ead47ce37ad41ff3922f91801c1b9aabc401797
Frankeo <ingfrancisco.moreno1@gmail.com>
2024-09-22 03:19:25 +0200

    test re order instructions for windows

--- installations/windows-full.ps1
+++ installations/windows-full.ps1
@@ -2,6 +2,12 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+# Install Docker Desktop
+choco install docker-desktop --version=4.12.0 -y
+
+# Install VS Code
+choco install vscode -y
+
 # Install Google Chrome
 choco install googlechrome -y
 
@@ -11,7 +17,7 @@ choco install hyper -y
 # Install Git and Git Bash
 choco install git -y
 
-# Clone dorfiles repo
+# Clone Dotfiles repo
 Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c', '"git clone https://github.com/Frankeo/dotfiles.git ~/dotfiles"') -NoNewWindow -Wait
 
 # Install Oh My Zsh
@@ -19,12 +25,11 @@ Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c
 Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c', '"cp ~/dotfiles/oh-my-zsh/.bashrc ~/"') -NoNewWindow -Wait
 Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c', '"echo yes | sh -c \"$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\""') -NoNewWindow -Wait
 
-# Setup Configs
-New-Item -ItemType SymbolicLink -Path "~\.gitconfig" -Target "~\dotfiles\git\.gitconfig"
+# Setup Hyper
 New-Item -Path "~\AppData\Roaming" -Name "Hyper" -ItemType "directory"
 New-Item -ItemType SymbolicLink -Path "~\AppData\Roaming\Hyper\.hyper.js" -Target "~\dotfiles\hyper\.hyper-windows.js"
 
-# Setup Oh My Zsh config
+# Setup Oh My Zsh
 Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c', '"git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k"') -NoNewWindow -Wait
 Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c', '"git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"') -NoNewWindow -Wait
 Start-Process -FilePath "$env:ProgramFiles\Git\bin\bash.exe" -ArgumentList @('-c', '"git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions"') -NoNewWindow -Wait
@@ -49,11 +54,8 @@ foreach ($file in gci *.ttf)
 }
 cp *.ttf c:\windows\fonts\
 
-# Install VS Code
-choco install vscode -y
+# Setup Git
+New-Item -ItemType SymbolicLink -Path "~\.gitconfig" -Target "~\dotfiles\git\.gitconfig"
 
 # Install Common Tools
 choco install audacity vlc gimp -y
-
-# Install Docker Desktop
-choco install docker-desktop -y
